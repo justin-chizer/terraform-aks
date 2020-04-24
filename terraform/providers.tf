0643c4f1d2a0708ad5732c21ae5a30a630a2a20d
justin-chizer <justin.chizer@gmail.com>
2020-04-24 13:32:15 -0700

    quick update

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,6 +1,6 @@
 # Configure the Azure Provider
 provider "azurerm" {
-  version = "~> 2.6.0" #~>: pessimistic constraint operator, constraining both the oldest and newest version allowed. For example, ~> 0.9 is equivalent to >= 0.9, < 1.0, and ~> 0.8.4, is equivalent to >= 0.8.4, < 0.9
+  version = "~> 2.7.0" #~>: pessimistic constraint operator, constraining both the oldest and newest version allowed. For example, ~> 0.9 is equivalent to >= 0.9, < 1.0, and ~> 0.8.4, is equivalent to >= 0.8.4, < 0.9
   features {}          #Required
 }
 
