93c4935d71636639bf20d2f3590f20cfa3c44123
justin-chizer <justin.chizer@gmail.com>
2020-04-23 22:20:45 -0700

    Test without a backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
-# Using multiple workspaces:
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraformsa"
-    storage_account_name = "chizerterraform"
-    container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
-  }
-}
+# # Using multiple workspaces:
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "terraformsa"
+#     storage_account_name = "chizerterraform"
+#     container_name       = "tfstate"
+#     key                  = "prod.terraform.tfstate"
+#   }
+# }
