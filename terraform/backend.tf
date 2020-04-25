b48be0eee13250dca7bdf963a800c3693d59510c
justin-chizer <justin.chizer@gmail.com>
2020-04-25 15:01:52 -0700

    this time

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -5,9 +5,9 @@ terraform {
     storage_account_name = "chizerterraform"
     container_name       = "tfstate"
     key                  = "prod.terraform.tfstate"
-    subscription_id      = "${var.subscription_id}"
-    client_id            = "${var.client_id}"
-    client_secret        = "${var.client_secret}"
-    tenant_id            = "${var.tenant_id}"
+    subscription_id      = var.subscription_id
+    client_id            = var.client_id
+    client_secret        = var.client_secret
+    tenant_id            = var.tenant_id
   }
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,6 +2,10 @@
 provider "azurerm" {
   version = "~> 2.7.0" #~>: pessimistic constraint operator, constraining both the oldest and newest version allowed. For example, ~> 0.9 is equivalent to >= 0.9, < 1.0, and ~> 0.8.4, is equivalent to >= 0.8.4, < 0.9
   features {}          #Required
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 # Configure Azure AD Provider
